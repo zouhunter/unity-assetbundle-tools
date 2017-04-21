27b5a5577a2249eceab255df72d665fe0151e385
zouhangte <zouhangtezbm@126.com>
2017-04-21 08:12:57 +0800

    init add demo

---
+++ AssetBundle/Editor/BuilderWindow.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+namespace AssetBundle {
+    public class BuilderWindow : EditorWindow
+    {
+        public string assetBundleName;
+        public string buildpath = "./Assets/StreamingAssets/AssetBundle";
+        public BuildAssetBundleOptions buildOption = BuildAssetBundleOptions.None;
+        public BuildTarget buildTarget = BuildTarget.StandaloneWindows;
+        public bool genFile = false;
+        private IBulidCtrl buildCtrl;
+        
+        public bool IsSingle { get; set; }
+        private Object[] selectionGameObject;
+        private Object[] selectionMaterial;
+        private Object[] selectionTexture;
+
+        void OnEnable()
+        {
+            buildCtrl = new BulidController();
+            SelectionChanged();
+            Selection.selectionChanged += SelectionChanged;
+        }
+
+
+        void OnGUI()
+        {
+            if (IsSingle)
+            {
+                #region 打包选中
+                assetBundleName = EditorGUILayout.TextField("AbName", assetBundleName);
+                EditorGUILayout.BeginHorizontal();
+                buildpath = EditorGUILayout.TextField("ExportTo", buildpath);
+                if (GUILayout.Button("选择路径"))
+                {
+                    buildpath = EditorUtility.SaveFolderPanel("选择保存路径", Application.streamingAssetsPath,"");
+                }
+                EditorGUILayout.EndHorizontal();
+
+                buildTarget = (BuildTarget)EditorGUILayout.EnumPopup("BuildTarget", buildTarget);
+
+                if (GUILayout.Button("BuildGameObjects"))
+                {
+                    buildCtrl.BuildSelectAssets(assetBundleName,buildpath, selectionGameObject, buildTarget);
+                }
+                else if (GUILayout.Button("BuildMaterials"))
+                {
+                    buildCtrl.BuildSelectAssets(assetBundleName, buildpath, selectionMaterial, buildTarget);
+                }
+                else if (GUILayout.Button("BuildTextures"))
+                {
+                    buildCtrl.BuildSelectAssets(assetBundleName, buildpath, selectionTexture, buildTarget);
+                }
+                #endregion
+            }
+            else
+            {
+                #region 全局打包
+                buildpath = EditorGUILayout.TextField("ExportTo", buildpath);
+                buildOption = (BuildAssetBundleOptions)EditorGUILayout.EnumMaskField("Options", buildOption);
+                buildTarget = (BuildTarget)EditorGUILayout.EnumPopup("BuildTarget", buildTarget);
+                genFile = EditorGUILayout.Toggle("GenFile", genFile);
+                if (GUILayout.Button("GlobleBulid"))
+                {
+                    buildCtrl.BuildGlobalAssetBundle(buildpath, buildOption, buildTarget, genFile);
+                }
+                #endregion
+            }
+        }
+        void SelectionChanged()
+        {
+            selectionGameObject = Selection.GetFiltered(typeof(GameObject), SelectionMode.DeepAssets);
+            selectionMaterial = Selection.GetFiltered(typeof(Material), SelectionMode.DeepAssets);
+            selectionTexture = Selection.GetFiltered(typeof(Texture), SelectionMode.DeepAssets);
+
+            if (Selection.activeObject!=null)
+            {
+                assetBundleName = Selection.activeObject.name;
+            }
+        }
+    }
+}
---
+++ AssetBundle/Editor/BulidController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AssetBundle
+{
+    public interface IBulidCtrl
+    {
+        void BuildGlobalAssetBundle(string path, BuildAssetBundleOptions option, BuildTarget target, bool record);
+        void BuildSelectAssets(string abName, string path, UnityEngine.Object[] obj, BuildTarget target);
+    }
+    public class BulidController : IBulidCtrl
+    {
+        //private List<string> bundlefiles = new List<string>();
+
+        public void BuildGlobalAssetBundle(string path, BuildAssetBundleOptions option, BuildTarget target, bool record)
+        {
+            BuildAllAssetBundles(path, option, target);
+        }
+        public void BuildSelectAssets(string abName, string path, UnityEngine.Object[] obj, BuildTarget target)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            AssetBundleBuild[] builds = new AssetBundleBuild[1];
+            builds[0].assetBundleName = abName;
+            builds[0].assetNames = new string[obj.Length];
+
+            for (int i = 0; i < obj.Length; i++)
+            {
+                builds[0].assetNames[i] = AssetDatabase.GetAssetPath(obj[i]);
+            }
+            BuildPipeline.BuildAssetBundles(path, builds, BuildAssetBundleOptions.DeterministicAssetBundle, target);
+            AssetDatabase.Refresh();
+        }
+
+        void BuildAllAssetBundles(string path, BuildAssetBundleOptions option, BuildTarget target)
+        {
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            /*AssetBundleManifest manifest = */BuildPipeline.BuildAssetBundles(path, option, target);
+        }
+
+        //void GetTextFile(string buildpath)
+        //{
+        //    //string bundleText = buildpath + "/bundles.txt";
+        //    MD5TableParse.GetMD5CSV(buildpath, AppFixed.BundleCSV);
+        //    if (File.Exists(bundleText)) File.Delete(bundleText);
+        //    bundlefiles.Clear();
+        //    Recursive(buildpath);
+        //    FileStream fs = new FileStream(bundleText, FileMode.CreateNew);
+        //    StreamWriter sw = new StreamWriter(fs);
+        //    for (int i = 0; i < this.bundlefiles.Count; i++)
+        //    {
+        //        string file = this.bundlefiles[i];
+        //        //string ext = Path.GetExtension(file);
+        //        if (file.EndsWith(".meta") || file.Contains(".DS_Store")) continue;
+
+        //        string md5 = FileUtility.md5file(file);
+        //        string value = "";
+        //        /////从本地读取文件有问题
+        //        value = file.Replace(buildpath + "/", string.Empty);
+
+        //        sw.WriteLine(value + "|" + md5);
+        //    }
+        //    sw.Close(); fs.Close();
+        //    AssetDatabase.Refresh();
+        //}
+
+        ///// <summary>
+        ///// 遍历目录及其子目录
+        ///// </summary>
+        //void Recursive(string path)
+        //{
+        //    string[] names = Directory.GetFiles(path);
+        //    string[] dirs = Directory.GetDirectories(path);
+        //    foreach (string filename in names)
+        //    {
+        //        string ext = Path.GetExtension(filename);
+        //        if (ext.Equals(".meta")) continue;
+        //        bundlefiles.Add(filename.Replace('\\', '/'));
+        //    }
+        //    foreach (string dir in dirs)
+        //    {
+        //        Recursive(dir);
+        //    }
+        //}
+    }
+}
+
