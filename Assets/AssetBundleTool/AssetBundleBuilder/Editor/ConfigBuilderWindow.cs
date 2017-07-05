d820be1ec98d3ee5c6ddf1d180800a65dd1163ae
zouhangte <zouhangtezbm@126.com>
2017-07-05 15:05:32 +0800

    添加利用配制表打包的窗口

---
+++ Assets/AssetBundleTool/AssetBundleBuilder/Editor/ConfigBuilderWindow.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AssetBundleBuilder
+{
+    public class ConfigBuilderWindow : EditorWindow
+    {
+        [MenuItem(ABBUtility.Menu_ConfigBuildWindow)]
+        static void BuildSingleAssetBundle()
+        {
+            EditorWindow.GetWindow<ConfigBuilderWindow>("局部AssetBundle", true);
+        }
+
+    }
+
+}
